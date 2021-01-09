2ede39f5f19eb216a7741c22559ade5369b71fd3
Proger-Maxwell <proger.maxwell@gmail.com>
2021-01-09 10:50:20 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+
+//alert('Hello!');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+/*const answer = +prompt('Do you have 18?','15');
+if (answer >= 18){
+    alert('Hello');
+}else{
+    alert('Sayonara');
+}
+    
+
+console.log(answer+5);*/
+
+const answers = [];
+
+/*answers[0] = prompt('You name?', '');
+answers[1] = prompt('You sur?', '');
+answers[2] = prompt('You age?', '');
+
+document.write(answers);
+console.log(typeof(answers));
+console.log(typeof(null));
+
+const category = 'toys';
+
+console.log('https://url.com/' + category);
+console.log(`https://url.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert (`Hello ${user}`);
+
+console.log(`arr` + 7);
+
+let incr = 10;
+let decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);*/
+
+
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
